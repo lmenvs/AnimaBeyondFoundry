11bd2e5072741e2a42cb4433cef0fba5bd8ddf49
Roger O <rgrrtz@gmail.com>
2023-12-27 20:22:21 +0100

    Migrations for new split projection and new act/innate magic system

--- src/module/migration/migrations/index.js
+++ src/module/migration/migrations/index.js
@@ -1,1 +1,3 @@
 export { Migration1DefaultFumble } from './1-fix-defaul-fumble';
+export { Migration2MagicProjection } from './2-fix-magic-projection';
+export { Migration3AlternativeAct } from './3-fix-alternative-act';
